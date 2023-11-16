4ef231c078f9db939aff806652b6cfc4d0c68594
Jetsurf <crmeade@mtu.edu>
2023-11-15 20:33:39 -0500

    Pull build

--- build/lib/fwsystems/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from . import __version__
-
-class FWSystemsConfig(AppConfig):
-	default_auto_field = 'django.db.models.BigAutoField'
-	name = "fwsystems"
-	verbose_name=f"AA FW Systems Check v{__version__}"
