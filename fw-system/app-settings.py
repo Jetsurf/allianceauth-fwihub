018c4ea66eb9849af121ef50a5a532b4cbf3120b
Jetsurf <crmeade@mtu.edu>
2023-11-15 20:30:47 -0500

    Initial FW Stats pull into DB

--- fw-system/app-settings.py
+++
@@ -1,9 +0,0 @@
-import re
-from django.apps import apps
-from django.conf import settings
-
-FWSYSTEMS_UPDATE_SYSTEMS = getattr(settings, "FWSYSTEMS_UPDATE_SYSTEMS", 3)
-FWSYSTEMS_ERROR_COLOR = getattr(settings, "FWSYSTEMS_ERROR_COLOR", 16711710)
-FWSYSTEMS_WARNING_COLOR = getattr(settings, "FWSYSTEMS_WARNING_COLOR", 14177041)
-FWSYSTEMS_INFO_COLOR = getattr(settings, "FWSYSTEMS_INFO_COLOR", 42751)
-FWSYSTEMS_SUCCESS = getattr(settings, "FWSYSTEMS_SUCCESS_COLOR", 6684416)
